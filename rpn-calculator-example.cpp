b07cbcb5effdc6bbe5a9fdf7e7988f2f6f151f61
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2025-09-24 21:52:33 -0700

    right shift

--- rpn-calculator-example.cpp
+++ rpn-calculator-example.cpp
@@ -78,6 +78,14 @@ shared_ptr<uint16_t> rpn_calc(command const cmd, uint16_t const value = 0) {
                 stack.push_back(numSums);
                 return make_shared<uint16_t>(numSums);
             }
+        case cmd_right_shift:
+            if(stack.size() >= 2) {
+                uint16_t firstNum = stack.back(); stack.pop_back();
+                uint16_t secondNum = stack.back(); stack.pop_back();
+                uint16_t numSums = secondNum >> firstNum;
+                stack.push_back(numSums);
+                return make_shared<uint16_t>(numSums);
+            }
     uint16_t val = 0b1001100100000011;
     shared_ptr<uint16_t> result = make_shared<uint16_t>(val);
     return result;
